4240dafcdf0662fcdc0ec0b08aba6b30ca539676
nduagoziem <162841062+nduagoziem@users.noreply.github.com>
2025-08-31 11:32:50 +0100

    changed the file for customers' auth and credentials from api.php to web.php

--- routes/api.php
+++ routes/api.php
@@ -6,17 +6,10 @@ use App\Http\Controllers\PhoneController;
 use App\Http\Controllers\TabletController;
 use App\Http\Controllers\ComputerController;
 use App\Http\Controllers\AccessoriesController;
-use App\Http\Controllers\Auth\CustomerController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::middleware("web")->group(function () {
-    Route::post("/register", [CustomerController::class, "register"])->name("customer.register");
-    Route::post("/login", [CustomerController::class, "login"])->name("customer.login");
-    // Route::post("/logout", [CustomerController::class, "logout"])->name("customer.logout");
-});
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum')
 
 Route::get('/phones', [PhoneController::class, 'get'])->name('phones.get');
 Route::get('/pcs', [ComputerController::class, 'get'])->name('pcs.get');
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\CustomerController;
+use App\Http\Controllers\TestController;
+
+Route::post("/customer/register", [CustomerController::class, "register"])->name("customer.register")->middleware("guest:customer");
+
+Route::post("/customer/login", [CustomerController::class, "login"])->name("customer.login")->middleware("guest:customer");
+
+Route::post("/customer/logout", [CustomerController::class, "logout"])->name("customer.logout")->middleware("auth:customer");
+
+Route::get('/customer', [CustomerController::class, "getLoggedInCustomer"])->middleware('auth:customer');
+
+// Route::get('/customer', [CustomerController::class, "geCustomerDetails"])->middleware('auth:customer');
+
+Route::get('/test', [TestController::class, "myMax"])->name("test");
